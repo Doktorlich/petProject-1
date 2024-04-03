d252ab8c1bd807cf0fe6957c108b28d246699bf4
Doktorlich <sergei199522@mail.ru>
2024-04-03 10:47:30 +0300

    Fix animation in "modal window"

--- src/js/modal-widow.js
+++ src/js/modal-widow.js
@@ -2,27 +2,32 @@ const contactBtn = document.querySelector(".contact__btn");
 const modalWindow = document.querySelector(".modal-window");
 const overlay = document.querySelector(".overlay");
 const close = document.querySelector(".modal-window__exit");
+const animPlay = document.querySelector(".animation-play");
 
 contactBtn.addEventListener("click", e => {
     e.preventDefault();
     modalWindow.classList.remove("hidden");
     overlay.classList.remove("hidden");
+    modalWindow.classList.add("animation-play");
 });
 
 close.addEventListener("click", e => {
     e.preventDefault();
     modalWindow.classList.add("hidden");
     overlay.classList.add("hidden");
+    modalWindow.classList.remove("animation-play");
 });
 
 overlay.addEventListener("click", e => {
     e.preventDefault();
     modalWindow.classList.add("hidden");
     overlay.classList.add("hidden");
+    modalWindow.classList.remove("animation-play");
 });
 document.addEventListener("keydown", e => {
     if (e.key === "Escape") {
         modalWindow.classList.add("hidden");
         overlay.classList.add("hidden");
+        modalWindow.classList.remove("animation-play");
     }
 });
